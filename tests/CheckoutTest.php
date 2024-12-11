9109e1f1baf8f2e8f73ebfecebee7f979eff5dc8
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-12-11 22:38:08 +0300

    Update CheckoutTest.php

--- tests/CheckoutTest.php
+++ tests/CheckoutTest.php
@@ -7,8 +7,10 @@ use OnlineStore\Checkout;
 use OnlineStore\Exceptions\InsufficientFundsException;
 use OnlineStore\Exceptions\PaymentGatewayException;
 
-class CheckoutTest extends TestCase {
-    public function testFinalizeOrderSuccess() {
+class CheckoutTest extends TestCase
+{
+    public function testFinalizeOrderSuccess(): void
+    {
         $product1 = new Product("Laptop", 1000, 5);
         $product2 = new Product("Smartphone", 500, 10);
         $cart = new Cart();
@@ -20,7 +22,8 @@ class CheckoutTest extends TestCase {
         $checkout->finalizeOrder();
     }
 
-    public function testFinalizeOrderInsufficientFunds() {
+    public function testFinalizeOrderInsufficientFunds(): void
+    {
         $product1 = new Product("Laptop", 1000, 5);
         $product2 = new Product("Smartphone", 500, 10);
         $cart = new Cart();
@@ -28,11 +31,12 @@ class CheckoutTest extends TestCase {
         $cart->addItem($product2, 1);
         $checkout = new Checkout($cart, 2000);
         $checkout->setPaymentMethod("credit card");
-        $this->expectOutputString("Ошибка: Недостаточно средств\n");
+        $this->expectOutputString("ошибка: Недостаточно средств\n");
         $checkout->finalizeOrder();
     }
 
-    public function testFinalizeOrderPaymentGatewayError() {
+    public function testFinalizeOrderPaymentGatewayError(): void
+    {
         $product1 = new Product("Laptop", 1000, 5);
         $product2 = new Product("Smartphone", 500, 10);
         $cart = new Cart();
@@ -40,7 +44,7 @@ class CheckoutTest extends TestCase {
         $cart->addItem($product2, 1);
         $checkout = new Checkout($cart, 3000);
         $checkout->setPaymentMethod("credit card");
-        $this->expectOutputString("Ошибка: Недостаточно средств\n");
+        $this->expectOutputString("ошибка: Ошибка платежа\n");
         $checkout->finalizeOrder();
     }
 }
