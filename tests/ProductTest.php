1974db915c0f1be9286d6fe6534f7836fac4041a
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-12-11 22:38:47 +0300

    Update ProductTest.php

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -4,29 +4,35 @@ use PHPUnit\Framework\TestCase;
 use OnlineStore\Product;
 use OnlineStore\Exceptions\OutOfStockException;
 
-class ProductTest extends TestCase {
-    public function testGetName() {
+class ProductTest extends TestCase
+{
+    public function testGetName(): void
+    {
         $product = new Product("Laptop", 1000, 5);
         $this->assertEquals("Laptop", $product->getName());
     }
 
-    public function testGetPrice() {
+    public function testGetPrice(): void
+    {
         $product = new Product("Laptop", 1000, 5);
         $this->assertEquals(1000, $product->getPrice());
     }
 
-    public function testGetStock() {
+    public function testGetStock(): void
+    {
         $product = new Product("Laptop", 1000, 5);
         $this->assertEquals(5, $product->getStock());
     }
 
-    public function testReduceStock() {
+    public function testReduceStock(): void
+    {
         $product = new Product("Laptop", 1000, 5);
         $product->reduceStock(2);
         $this->assertEquals(3, $product->getStock());
     }
 
-    public function testReduceStockException() {
+    public function testReduceStockException(): void
+    {
         $product = new Product("Laptop", 1000, 5);
         $this->expectException(OutOfStockException::class);
         $product->reduceStock(6);
