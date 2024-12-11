f6e06570d30ffc42845f30060dbf959cf4c3169c
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-12-11 22:33:59 +0300

    Update Card.php

--- src/Card.php
+++ src/Card.php
@@ -5,11 +5,13 @@ namespace OnlineStore;
 use OnlineStore\Exceptions\ItemOutOfStockException;
 use OnlineStore\Exceptions\CartLimitExceededException;
 
-class Cart {
-    private $items = [];
-    private $maxItems = 20;
+class Cart
+{
+    private array $items = [];
+    private int $maxItems = 20;
 
-    public function addItem(Product $product, $quantity) {
+    public function addItem(Product $product, int $quantity)
+    {
         if (count($this->items) >= $this->maxItems) {
             throw new CartLimitExceededException("Превышен лимит корзины");
         }
@@ -23,12 +25,14 @@ class Cart {
         $this->items[$product->getName()]['product']->reduceStock($quantity);
     }
 
-    public function removeItem(Product $product) {
+    public function removeItem(Product $product)
+    {
         unset($this->items[$product->getName()]);
     }
 
-    public function getTotal() {
-        $total = 0;
+    public function getTotal(): float
+    {
+        $total = 0.0;
         foreach ($this->items as $item) {
             $total += $item['product']->getPrice() * $item['quantity'];
         }
